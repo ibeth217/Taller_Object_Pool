5d4d3e5b2b4b41e57749f861ddccd5f27b681a35
john.quimbaya <ibeth.pacheco.dlh@gmail.com>
2025-03-18 21:17:17 -0500

    ajuste Main

--- src/co/edu/ucc/ibeth/medicina/Main.java
+++ src/co/edu/ucc/ibeth/medicina/Main.java
@@ -1,5 +1,11 @@
 package co.edu.ucc.ibeth.medicina;
-
+/**
+ * Encabezado Personalizado:
+ * Autor: Ibeth Pacheco de la Hostia
+ * Curso: Patrones de Diseño Orientado a Objetos 
+ * Fecha: 18/03/2025
+ * Descripción: Clase principal que utiliza el Objectpool
+ */
 public class Main {
     public static void main(String[] args) {
         PoolCamillas pool = new PoolCamillas(3);
